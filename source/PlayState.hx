fd049bd21ed0e8c1a1543547d20acfffa076524a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-23 15:06:55 -0500

    objects array n shit

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,24 @@
 package;
 
+import Object.ObjectCreateEvent;
+import Object.ObjectUpdateEvent;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var objects:Array<Object> = [];
+
 	override public function create()
 	{
 		super.create();
+		for (object in objects)
+			object.onCreate(new ObjectCreateEvent(object));
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		for (object in objects)
+			object.onUpdate(new ObjectUpdateEvent(object, elapsed));
 	}
 }
