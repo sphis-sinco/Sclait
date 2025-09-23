738a6b8bd52ae3d856d775a4cb25a921e320b35e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-23 15:14:23 -0500

    mod loadin

--- source/Main.hx
+++ source/Main.hx
@@ -2,6 +2,9 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import polymod.Polymod.PolymodError;
+import polymod.Polymod;
+import sys.FileSystem;
 
 class Main extends Sprite
 {
@@ -9,6 +12,32 @@ class Main extends Sprite
 	{
 		super();
 
+		var sysMods = [];
+
+		#if sys
+		for (mod in FileSystem.readDirectory('mods/'))
+		{
+			if (FileSystem.isDirectory('mods/$mod'))
+				sysMods.push(mod);
+		}
+		#end
+
+		loadMods(sysMods);
 		addChild(new FlxGame(0, 0, PlayState));
+	}
+	private function loadMods(dirs:Array<String>)
+	{
+		var results = Polymod.init({
+			modRoot: 'mods/',
+			dirs: dirs,
+			errorCallback: onError,
+			ignoredFiles: Polymod.getDefaultIgnoreList(),
+			useScriptedClasses: true,
+		});
+	}
+
+	private function onError(error:PolymodError)
+	{
+		trace('[${error.severity}] (${Std.string(error.code).toUpperCase()}): ${error.message}');
 	}
 }
