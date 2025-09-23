c80378291a08ec0a39fc3865fba526a41bf5c221
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-23 18:02:33 -0500

    connect to other module vars

---
+++ assets/scripts/tests/LinkingModule.hx
@@ -0,0 +1,22 @@
+import events.CreateEvent;
+import events.FocusEvent;
+import events.UpdateEvent;
+import modules.Module;
+import modules.ModuleHandler;
+
+class LinkingModule extends Module
+{
+	override public function new()
+	{
+		super('linking-module');
+	}
+
+	override function onFocusGained(event:FocusEvent)
+	{
+		super.onFocusGained(event);
+
+		var mtl = ModuleHandler.getModule('module-to-link');
+		mtl.scriptSet('linked', true);
+
+	}
+}
