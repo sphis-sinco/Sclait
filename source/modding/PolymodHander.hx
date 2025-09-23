6d15950897af571257616c3df5a3aafb5c49c7d8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-23 17:03:57 -0500

    i cant get it to accept .hx files :(

--- source/modding/PolymodHander.hx
+++ source/modding/PolymodHander.hx
@@ -1,6 +1,7 @@
 package modding;
 
 import polymod.Polymod;
+import polymod.format.ParseRules;
 
 class PolymodHander
 {
@@ -12,14 +13,47 @@ class PolymodHander
 		Polymod.addImportAlias('events.UpdateEvent', events.UpdateEvent);
 	}
 
+	public static function buildParseRules():polymod.format.ParseRules
+	{
+		var output:polymod.format.ParseRules = polymod.format.ParseRules.getDefault();
+		// Ensure TXT files have merge support.
+		output.addType('txt', TextFileFormat.LINES);
+		// Ensure script files have merge support.
+		output.addType('hscript', TextFileFormat.PLAINTEXT);
+		output.addType('hxs', TextFileFormat.PLAINTEXT);
+		output.addType('hxc', TextFileFormat.PLAINTEXT);
+		output.addType('hx', TextFileFormat.PLAINTEXT);
+
+		// You can specify the format of a specific file, with file extension.
+		// output.addFile("data/introText.txt", TextFileFormat.LINES)
+		return output;
+	}
+
+	public static function buildIgnoreList():Array<String>
+	{
+		var result = Polymod.getDefaultIgnoreList();
+
+		result.push('.git');
+		result.push('.gitignore');
+		result.push('.gitattributes');
+		result.push('README.md');
+
+		result.push('.haxelib');
+
+		return result;
+	}
+
 	public static function loadMods(dirs:Array<String>)
 	{
 		var results = Polymod.init({
 			modRoot: 'mods/',
 			dirs: dirs,
 			errorCallback: onError,
-			ignoredFiles: Polymod.getDefaultIgnoreList(),
+			ignoredFiles: buildIgnoreList(),
 			useScriptedClasses: true,
+			loadScriptsAsync: #if html5 true #else false #end,
+			framework: OPENFL,
+			parseRules: buildParseRules()
 		});
 	}
 
